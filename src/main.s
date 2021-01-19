8f26382172148b2ba3cbd5d8c19bfa12aec633ff
Wendel Scardua <wendelscardua@gmail.com>
2021-01-19 17:12:35 -0300

    First commit (using Famitone)

---
+++ src/main.s
@@ -0,0 +1,274 @@
+.include "constants.inc"
+.include "header.inc"
+.include "charmap.inc"
+.include "vram-buffer.inc"
+
+.linecont +
+
+; famitone2 config
+FT_PAL_SUPPORT=0
+FT_NTSC_SUPPORT=1
+FT_SFX_ENABLE=1
+FT_THREAD=1
+FT_DPCM_ENABLE=0
+FT_SFX_STREAMS=4
+FT_DPCM_OFF=$c000
+
+; music/sfx constants
+.enum music_track
+  PlaceholderTrack
+.endenum
+
+.enum sfx
+  PlaceholderSFX
+.endenum
+
+.macro SFX effect, channel
+  save_regs
+  LDA #sfx::effect
+  LDX #.ident ( .concat( "FT_SFX_", .string(channel) ) )
+  JSR FamiToneSfxPlay
+  restore_regs
+.endmacro
+
+.macro PLAY track
+.local skip
+  save_regs
+  LDA #music_track::track
+  JSR FamiToneMusicPlay
+  restore_regs
+.endmacro
+
+.macro SCREEN_OFF
+  LDA #$00
+  STA PPUCTRL ; disable NMI
+  STA PPUMASK ; disable rendering
+.endmacro
+
+.macro SCREEN_ON
+  LDA #%10001000  ; turn on NMIs, sprites use second pattern table
+  STA PPUCTRL
+  LDA #%00011110  ; turn on screen
+  STA PPUMASK
+.endmacro
+
+.segment "ZEROPAGE"
+FT_TEMP: .res 3
+.segment "FAMITONE"
+FT_BASE_ADR: .res 186
+.segment "CODE"
+.include "famitone2.s"
+
+.segment "OAM"
+.struct Sprite
+  ycoord .byte
+  tile .byte
+  flag .byte
+  xcoord .byte
+.endstruct
+
+oam_sprites:
+  .repeat 64
+    .tag Sprite
+  .endrepeat
+.zeropage
+
+.importzp buttons
+.importzp last_frame_buttons
+.importzp released_buttons
+.importzp pressed_buttons
+.importzp rle_ptr
+
+; zp vars
+addr_ptr: .res 2 ; generic address pointer
+ppu_addr_ptr: .res 2
+sprite_ptr: .res 2 ; metasprite pointer
+
+.export nmis
+.export old_nmis
+
+nmis: .res 1
+old_nmis: .res 1
+
+sprite_counter: .res 1
+
+; temp stuff
+temp_x: .res 1
+temp_y: .res 1
+temp_acc: .res 1
+temp_tile: .res 1
+temp_flag: .res 1
+
+.segment "BSS"
+; non-zp RAM goes here
+
+.segment "CODE"
+
+.import reset_handler
+.import readjoy
+.import unrle
+
+.import music_data
+.import sfx_data
+
+.macro KIL ; pseudo instruction to kill the program
+  .byte $12
+.endmacro
+
+.macro VBLANK
+  .local vblankwait
+vblankwait:
+  BIT PPUSTATUS
+  BPL vblankwait
+.endmacro
+
+.macro save_regs
+  PHA
+  TXA
+  PHA
+  TYA
+  PHA
+.endmacro
+
+.macro restore_regs
+  PLA
+  TAY
+  PLA
+  TAX
+  PLA
+.endmacro
+
+.proc irq_handler
+  RTI
+.endproc
+
+.proc nmi_handler
+  save_regs
+  INC nmis
+  JSR flush_vram_buffer
+  ; reset ppuaddr
+  BIT PPUSTATUS
+  JSR set_scroll
+  JSR refresh_oam
+  JSR FamiToneUpdate
+  restore_regs
+  RTI
+.endproc
+
+.export main
+.proc main
+  SEI         ; ignore IRQs
+  CLD         ; disable decimal mode
+  LDX #$40
+  STX $4017   ; disable APU frame IRQ
+  LDX #$ff
+  TXS         ; Set up stack
+  INX         ; now X = 0
+  STX PPUCTRL ; disable NMI
+  STX PPUMASK ; disable rendering
+  STX $4010   ; disable DMC IRQs
+
+  LDX #0
+clear_ram:
+  LDA #$00
+  STA $0000,X
+  STA $0100,X
+  STA $0300,X
+  STA $0400,X
+  STA $0500,X
+  STA $0600,X
+  STA $0700,X
+  LDA #$fe
+  STA $0200,X
+  INX
+  BNE clear_ram
+
+  reset_vram_stack
+
+  SCREEN_ON
+
+  LDX #<music_data
+  LDY #>music_data
+  LDA #1
+  JSR FamiToneInit
+
+  ; init FamiTone SFX
+  LDX #<sfx_data
+  LDY #>sfx_data
+  LDA #1
+  JSR FamiToneSfxInit
+
+  PLAY PlaceholderTrack
+
+forever:
+  LDA nmis
+  CMP old_nmis
+  BEQ etc
+  STA old_nmis
+  ; new frame code
+  JSR readjoy
+etc:
+  JMP forever
+.endproc
+
+.proc refresh_oam
+  ; Refresh OAM
+  LDA #$00
+  STA OAMADDR
+  LDA #$02
+  STA OAMDMA
+  RTS
+.endproc
+
+.proc set_scroll
+  LDA PPUSTATUS
+  LDA #%10001000  ; turn on NMIs, sprites use second pattern table; reset nametable
+  STA PPUCTRL  
+  LDA #$00
+  STA PPUSCROLL
+  STA PPUSCROLL
+  RTS
+.endproc
+
+.proc load_palettes
+  ; cobbles Y
+  LDY PPUSTATUS
+  LDY #$3f
+  STY PPUADDR
+  LDY #$00
+  STY PPUADDR
+:
+  LDA palettes,Y
+  STA PPUDATA
+  INY
+  CPY #$20
+  BNE :-
+  RTS
+.endproc
+
+.proc erase_remaining_sprites
+  LDX sprite_counter
+  LDA #$F0
+:
+  STA oam_sprites+Sprite::ycoord, X
+  .repeat .sizeof(Sprite)
+  INX
+  .endrepeat
+  BNE :-
+  RTS
+.endproc
+
+.segment "VECTORS"
+.addr nmi_handler, reset_handler, irq_handler
+
+.segment "RODATA"
+
+palettes:
+.incbin "../assets/bg-palettes.pal"
+.incbin "../assets/sprite-palettes.pal"
+
+nametable_title: .incbin "../assets/nametables/title.rle"
+
+.segment "CHR"
+.incbin "../assets/chr/background-4k.chr"
+.incbin "../assets/chr/sprites-4k.chr"
---
+++ src/vram-buffer.s
@@ -0,0 +1,88 @@
+.include "constants.inc"
+
+.export vram_buffer
+.export vram_buffer_sp
+.export main_sp
+.export flush_vram_buffer
+.export _tens
+.export _ones
+
+.segment "ZEROPAGE"
+
+.import nmis
+.import old_nmis
+
+vram_buffer_sp: .res 1
+main_sp: .res 1
+str_ptr: .res 2
+
+.segment "STACK"
+vram_buffer: .res 128
+
+.segment "CODE"
+
+.proc flush_vram_buffer
+  LDA vram_buffer_sp
+  BNE :+
+  RTS
+:
+  TSX
+  STX main_sp
+  LDX #0
+  STX vram_buffer_sp
+  DEX
+  TXS
+
+  BIT PPUSTATUS
+loop:
+  PLA
+  BEQ exit_loop
+  CMP #$80
+  BCS string
+  STA PPUADDR
+  PLA
+  STA PPUADDR
+mini_loop:
+  PLA
+  BEQ loop
+  STA PPUDATA
+  JMP mini_loop
+string:
+  AND #%00111111
+  STA PPUADDR
+  PLA
+  STA PPUADDR
+  PLA
+  STA str_ptr
+  PLA
+  STA str_ptr+1
+  LDY #0
+strloop:
+  LDA (str_ptr), Y
+  BEQ loop
+  STA PPUDATA
+  INY
+  JMP strloop
+exit_loop:
+  LDX main_sp
+  TXS
+
+  BIT PPUSTATUS
+  LDA #$20
+  STA PPUADDR
+  LDA #$00
+  STA PPUADDR
+
+  RTS
+.endproc
+
+.segment "RODATA"
+
+_tens:
+.repeat 100, i
+  .byte (i / 10) + $10
+.endrepeat
+_ones:
+.repeat 100, i
+  .byte (i .mod 10) + $10
+.endrepeat
